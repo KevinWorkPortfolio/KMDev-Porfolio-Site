64d96326a68b31f030966e8e55ed4e7bb937eda9
Kevin Marinucci <102044480+KevinWorkPortfolio@users.noreply.github.com>
2026-04-06 17:32:51 +0200

    Update loadProjects.js

--- loadProjects.js
+++ loadProjects.js
@@ -1,20 +1,25 @@
-const container = document.querySelector('.projects-container');
+const container = document.querySelector('.project-grid');
 
-fetch('projects.json')
-  .then(res => res.json())
-  .then(projects => {
-    projects.forEach(p => {
-      container.innerHTML += `
-        <div class="project-card">
-          <img src="${p.image}" alt="${p.title}">
-          <div class="project-content">
-            <h3>${p.title}</h3>
-            <div class="project-links">
-              <a href="${p.github}" target="_blank" class="btn">GitHub Repo</a>
-              <a href="${p.download}" target="_blank" class="btn">Download ZIP</a>
+if (!container) {
+  console.error("Container .project-grid non trovato!");
+} else {
+  fetch('projects.json')
+    .then(res => res.json())
+    .then(projects => {
+      projects.forEach(p => {
+        container.innerHTML += `
+          <div class="project-card">
+            <img src="${p.image}" alt="${p.title}">
+            <div class="project-content">
+              <h3>${p.title}</h3>
+              <div class="project-links">
+                <a href="${p.github}" target="_blank" class="btn">GitHub Repo</a>
+                <a href="${p.download}" target="_blank" class="btn">Download ZIP</a>
+              </div>
             </div>
           </div>
-        </div>
-      `;
-    });
-  });
+        `;
+      });
+    })
+    .catch(err => console.error("Errore nel caricamento:", err));
+}
